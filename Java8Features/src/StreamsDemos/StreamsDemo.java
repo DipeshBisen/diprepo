381ff8638a1953847a87256ecdda82a81aff8743
DipeshBisen <dipbisen@gmail.com>
2020-01-20 00:30:15 +0530

    Streams updated with filter() and map() method.

--- Java8Features/src/StreamsDemos/StreamsDemo.java
+++ Java8Features/src/StreamsDemos/StreamsDemo.java
@@ -1,3 +1,7 @@
+// if we want to process objects from the collections then we go for streams.
+// stream().filter(....)
+// stream().map(....)
+
 package StreamsDemos;
 
 import java.util.ArrayList;
@@ -23,9 +27,11 @@ public class StreamsDemo {
 		
 		List<Integer> flist=lst.stream().filter(i->i%2==0).collect(Collectors.toList());
 		
+		List<Integer> updatedList=lst.stream().map(i->i*100).collect(Collectors.toList());
+		
 		Stream<Integer> fstream=lst.stream().filter(i->i%2==0);
 		
-		
+		System.out.println("Updated list: "+updatedList);
 		System.out.println(flist);    //[10, 20, 30]
 		System.out.println(fstream);  //java.util.stream.ReferencePipeline$2@404b9385
 
