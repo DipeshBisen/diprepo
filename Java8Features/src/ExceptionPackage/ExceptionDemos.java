a486a71c08de70ec38b22cc40347d8accc322309
DipeshBisen <dipbisen@gmail.com>
2020-01-19 16:09:46 +0530

    some minor changes

--- Java8Features/src/ExceptionPackage/ExceptionDemos.java
+++ Java8Features/src/ExceptionPackage/ExceptionDemos.java
@@ -4,16 +4,16 @@ public class ExceptionDemos {
 
 	//static ArithmeticException ae;   //Runtime error.......... NullPointerException..
 	
-		static ArithmeticException ae=new ArithmeticException();
+	static ArithmeticException ae=new ArithmeticException();
 		
-		public static void main(String[] args) {
-			// TODO Auto-generated method stub
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 			
 			
-			//throw new ArithmeticException();
-			//System.out.println("hello");   //CompileTime Error ,,,,,,unreachable code.....
+		//throw new ArithmeticException();
+		//System.out.println("hello");   //CompileTime Error ,,,,,,unreachable code.....
 			
-			throw ae;   //Now it works properly....... We will get arithmetic exception.....
+		throw ae;   //Now it works properly....... We will get arithmetic exception.....
 
-		}
+	}
 }
