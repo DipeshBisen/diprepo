2589b1ddd67516b44f637c7792db1467db755dc5
Dipesh Bisen <Dipesh.Bisen@itiviti.com>
2020-01-21 18:38:35 +0530

    Multithreading demos.

---
+++ Java8Features/src/multiThreadingDemo/MultithreadingDemo.java
@@ -0,0 +1,17 @@
+//multithreading 
+
+//  methods that prevent thread execution : 1)yield() 
+								//			2)join()
+								//			3)sleep()
+
+
+package multiThreadingDemo;
+
+public class MultithreadingDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
